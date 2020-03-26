3d101f0ffc58021b4f6fc7faa8421d5224592b8e
Maks Kaputa <51413113+MaximKaputa@users.noreply.github.com>
2020-03-26 22:14:03 +0200

    Updated step 3

--- Step 3/JSONStore.js
+++ Step 3/JSONStore.js
@@ -1,30 +1,65 @@
 Ext.ns('App.jsonStore');
 
-Ext.Ajax.request({
-    url: 'https://swapi.co/api/people',
-    success: function(response, options){
-     var json  = JSON.parse(response.responseText)
-        console.log(json.results);
-    },
-    failure: function(response, options){
-        alert("Ошибка: " + response.statusText);
-    }
-});  
+// var request = new XMLHttpRequest();
+// request.open('GET', 'https://swapi.co/api/people/ ');
+// request.responseType = 'json';
+// request.send();
+
+
+
+
+// Ext.Ajax.request({
+//     url: 'https://swapi.co/api/people',
+//     success: function(response, options){
+//      var json  = JSON.parse(response.responseText)
+//         console.log(json.results);
+//     },
+//     failure: function(response, options){
+//         alert("Ошибка: " + response.statusText);
+//     }
+// });
  
 
 
  App.jsonStore.createJsonStore = function () {
+
+     setTimeout(function () {
+        console.log(jsonStore.data)
+     },5000)
     var jsonStore = new Ext.data.JsonStore({
-        url: 'https://swapi.co/api/people/1',
+        //url: 'https://swapi.co/api/people',
+        root: 'results',
+        fields:[
+            'name'
+        ],
+        baseParams : {
+                start : 0,
+                limit : 100
+            },
+        remoteSort : true,
+        sortInfo: {},
+        proxy : new Ext.data.HttpProxy({
+            url : 'https://swapi.co/api/people'
+        }),
+        listeners:{
+            load: function (store) {
+                console.log('loaded',arguments)
+                console.log('1')
+            }
+        }
      });
-    console.log()
-      var grid = Ext.create({
-      });
+     jsonStore.load()
+    // console.log(request.response)
+    //  var bb = JSON.parse(JSON.stringify(request.response.results))
+    //  console.log( bb)
+
+     //  var grid = Ext.create({
+     //  });
       var gridWin = Ext.create({
          xtype: 'window',
-          height: 400,
-          layout: 'form',
-          items: grid
+         height: 400,
+         layout: 'form',
+         // items: grid
      })
-     //return gridWin
+     return gridWin
 }
